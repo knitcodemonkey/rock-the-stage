a688293f5b6cd4efd1863ff6bd2e90e62f1a72cf
Jen Luker <jluker@deseretdigital.com>
2018-05-26 13:47:44 -0600

    This is very specific to React Boston at the moment. We'll need to expand it to work for others, too.

--- src/talk-dropdown.js
+++ src/talk-dropdown.js
@@ -61,21 +61,51 @@ class TalkDropdown extends Component {
               //console.log(field.getAttribute("aria-label"));
               switch (field.getAttribute("aria-label")) {
                 case "Full name":
+                  field
+                    .setAttribute("data-initial-value", data.speaker.name || "")
+                    .setAttribute("bad-data", false);
                   field.value = data.speaker.name || "";
                   break;
                 case "Your email":
+                  field
+                    .setAttribute(
+                      "data-initial-value",
+                      data.speaker.email || ""
+                    )
+                    .setAttribute("bad-data", false);
                   field.value = data.speaker.email || "";
                   break;
                 case "Twitter":
+                  field
+                    .setAttribute(
+                      "data-initial-value",
+                      data.speaker.twitter || ""
+                    )
+                    .setAttribute("bad-data", false);
                   field.value = data.speaker.twitter || "";
                   break;
                 case "Github":
+                  field
+                    .setAttribute(
+                      "data-initial-value",
+                      data.speaker.github || ""
+                    )
+                    .setAttribute("bad-data", false);
                   field.value = data.speaker.github || "";
                   break;
                 case "Speaker photo (URL)":
+                  field
+                    .setAttribute(
+                      "data-initial-value",
+                      data.speaker.photo || ""
+                    )
+                    .setAttribute("bad-data", false);
                   field.value = data.speaker.photo || "";
                   break;
                 case "Talk title":
+                  field
+                    .setAttribute("data-initial-value", data.talk.title || "")
+                    .setAttribute("bad-data", false);
                   field.value = data.talk.title || "";
                   break;
                 default:
@@ -86,6 +116,27 @@ class TalkDropdown extends Component {
             document.querySelectorAll("textarea").forEach(function(field) {
               console.log(field.getAttribute("aria-label"));
               switch (field.getAttribute("aria-label")) {
+                case "Speaker bio (written in the 3rd person)":
+                  field
+                    .setAttribute("data-initial-value", data.speaker.bio || "")
+                    .setAttribute("bad-data", false);
+                  field.value = data.speaker.bio || "";
+                  break;
+                case "Talk abstract":
+                  field
+                    .setAttribute(
+                      "data-initial-value",
+                      data.talk.abstract || ""
+                    )
+                    .setAttribute("bad-data", false);
+                  field.value = data.talk.abstract || "";
+                  break;
+                case "Anything else we should know?":
+                  field
+                    .setAttribute("data-initial-value", data.talk.details || "")
+                    .setAttribute("bad-data", false);
+                  field.value = data.talk.details || "";
+                  break;
                 default:
                   field.value = "";
               }
@@ -113,7 +164,7 @@ class TalkDropdown extends Component {
             id="selectTalk"
             name="select-talk"
             aria-labelledby="select a talk"
-            onChange={this.handleChange}
+            onBlur={this.handleChange}
           >
             <option value={-1} key={`talk_none`}>
               Please select a talk
